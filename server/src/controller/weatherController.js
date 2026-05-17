ddf29fe049c2011ef2e31d58b0f8ad61397e8efc
Benenendyel <Vencarlostagaro@gmail.com>
2026-05-18 00:54:18 +0800

    Added hourly forecast feature

--- server/src/controller/weatherController.js
+++ server/src/controller/weatherController.js
@@ -4,6 +4,7 @@ const weatherController = {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHERMAP_API_KEY}&units=metric`,
     );
+
     const data = await response.json();
     res.json(data);
   },
@@ -13,6 +14,17 @@ const weatherController = {
     const response = await fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${process.env.WEATHERMAP_API_KEY}`,
     );
+
+    const data = await response.json();
+    res.json(data);
+  },
+
+  async getForecast(req, res) {
+    const cityName = req.query.cityName;
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${process.env.WEATHERMAP_API_KEY}&units=metric`,
+    );
+
     const data = await response.json();
     res.json(data);
   },
--- server/src/routes/weatherRouter.js
+++ server/src/routes/weatherRouter.js
@@ -4,5 +4,6 @@ const weatherController = require("../controller/weatherController");
 
 router.get("/api/weather", weatherController.getWeatherInformation);
 router.get("/api/search", weatherController.searchCities);
+router.get("api/forecast", weatherController.getForecast);
 
 module.exports = router;
